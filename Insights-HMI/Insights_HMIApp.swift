f937ec0b4260fe7f8fd3fcdda320213688661c9c
Devashish Upadhyay <devashish5113@gmail.com>
2026-04-24 14:27:07 +0530

    Initial Commit

---
+++ Insights-HMI/Insights_HMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insights_HMIApp.swift
+//  Insights-HMI
+//
+//  Created by Devashish Upadhyay on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Insights_HMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
